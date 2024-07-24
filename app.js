cff6335eee089d4012b660c8653dcc8df03d64a5
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2024-07-24 07:41:41 +0530

    Day 1 : add react using CDN

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement('h2', {}, 'Good morning everyone!!')
+const heading2 = React.createElement('h3', {}, 'Have a nice day.')
+ReactDOM.render([heading, heading2], document.getElementById('root'))
